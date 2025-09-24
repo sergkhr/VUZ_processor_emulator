c2f51ab1ee221fd75d58d26f71ac5db27e1e789b
sergkhr <46621907+sergkhr@users.noreply.github.com>
2025-09-24 18:58:35 +0300

    upd registries an commands

--- emulator_code.py
+++ emulator_code.py
@@ -19,35 +19,56 @@ registry = {
 }
 
 
+memory = dict()
+
+
 
 # Флаги
 ZF = 0
 
 
-
 # Регистры команд
-# Я чет забыл а это надо или...
+command_registry = {
+    "MOV" : 0,
+    "MOV_OFFSET" : 1,
+    "ADD" : 2,
+    "CMP" : 3,
+    "JMP" : 4,
+    "JZ" : 5,
+    "JNZ" : 6,
+}
 
 PC = 1 #1 же?
 
 # Команды
 def call_command(command, res_address, first_operand, second_operand):
+    global command_registry
     match command:
-        case "MOV":
+        case command_registry.get("MOV"):
             MOV(res_address, first_operand)
-        case "ADD":
+        case command_registry.get("MOV_OFFSET"):
+            MOV_OFFSET(res_address, first_operand, second_operand)
+        case command_registry.get("ADD"):
             ADD(res_address, first_operand, second_operand)
-        case "CMP":
+        case command_registry.get("CMP"):
             CMP(first_operand, second_operand)
-        case "JZ":
+        case command_registry.get("JMP"):
+            JMP(res_address)
+        case command_registry.get("JZ"):
             JZ(res_address)
-        case "JNZ":
+        case command_registry.get("JNZ"):
             JNZ(res_address)
+        case _:
+            print("ERROR, lohundra")
 
 
 
 def MOV(res_address, operand):
     res_address = operand
+
+
+def MOV_OFFSET(res_address, operand, offset):
+    res_address = operand[offset]
 
 
 # Для АЛУ
@@ -72,11 +93,17 @@ def CMP(first_operand, second_operand): # а мы оставляем тут пе
         ZF = 0
 
 
+def JMP(where_to_jump):
+    global PC
+    PC = where_to_jump
+
+
 def JZ(where_to_jump): # а нам тут надо 3 адреса?
     global ZF
     global PC
     if(ZF == 1):
         PC = where_to_jump
+
 
 def JNZ(where_to_jump):
     global ZF
